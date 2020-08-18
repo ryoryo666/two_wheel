8223410f837085e152c52ba14f9ade33c9a95763
ryosakamoto666 <sakaryo0102@gmail.com>
2020-08-18 17:41:55 +0900

    home

--- src/move_gazebo.py
+++ src/move_gazebo.py
@@ -3,8 +3,29 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from two_wheel.msg import curve_data
+
+V=0.4
+R=0    #default
+
+def callback(data):
+    if data.
 
 def pub():
     rospy.init_node("robot_twist_pub",anonymous=True)
+    rospy.Subscriber("Turning_info", curve_data, callback)
     pub=rospy.Publisher("/robo_gazebo/diff_drive_controller/cmd_vel", Twist, queue_size=10)
-    
+    r=rospy.Rate(10)
+
+    twist=Twist()
+    twist.linear.x=V
+    twist.angular.z=0
+
+    while not rospy.is_shutdown():
+        pub.publish(twist)
+        r.sleep()
+
+if __name__=="__main__":
+    try:
+        pub()
+    except rospy.ROSInterruptException: pass
