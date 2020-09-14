85b9c8d46d878e49183c544d6d4562163aac50c2
ryoryo666 <sakaryo0102@gmail.com>
2020-09-13 19:04:51 -0700

    school

--- csv/csv_plot.py
+++ csv/csv_plot.py
@@ -7,10 +7,13 @@ x=data[:,0]
 y=data[:,1]
 
 plt.plot(x,y,color="red")
-plt.title("PID control result")
-plt.xlabel("Time[s]")
-plt.ylabel("output")
+#plt.title("PID control result")
+plt.title("Target=30rpm Kp=0.18 Kd=0.085", fontsize=18)
+plt.xlabel("Time[s]", fontsize=18)
+plt.ylabel("Output[rpm]", fontsize=18)
 plt.xlim(0,)
-plt.ylim(0,)
+plt.ylim(0,40)
+plt.tick_params(labelsize=15)
 plt.grid()
 plt.show()
+
