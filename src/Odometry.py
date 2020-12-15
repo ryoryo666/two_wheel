593ec19e009b8c9831ae64e3a60011d3d8788399
ryoryo666 <sakaryo0102@gmail.com>
2020-12-15 17:31:29 +0900

    school

--- src/Odometry.py
+++ src/Odometry.py
@@ -16,23 +16,22 @@ def odom(msg):
 	now_time = rospy.Time.now()
 	dt = now_time - last_time
 	dt = dt.secs + dt.nsecs/10.0**9
-	
-	vr = round(R_data,2)*((2*math.pi)/60.0) * wr 
-	vl = round(L_data,2)*((2*math.pi)/60.0) * wr 
+
+	vr = round(R_data,2)*((2*math.pi)/60.0) * wr
+	vl = round(L_data,2)*((2*math.pi)/60.0) * wr
 	w = (vr-vl)/(2*d)
 
 	dL_r = vr*dt
 	dL_l = vl*dt
 	dth = w *dt
 	dL = (dL_r + dL_l)/2
-	
-	Odom.header.stamp.secs = now_time.secs
-	Odom.header.stamp.nsecs = now_time.nsecs
+
+	Odom.header.stamp.secs = now_time.secs - start_Time.secs
+	Odom.header.stamp.nsecs = now_time.nsecs - start_Time.nsecs
 
 	if dth < 0.000001 or dth==0.0 :
 		Odom.pose.pose.position.x = last_x + dL * math.cos(last_th+(dth/2))
 		Odom.pose.pose.position.y = last_y + dL * math.sin(last_th+(dth/2))
-
 	else :
 		p = dL/dth
 		dL1 = 2*p*math.sin(dth/2)
@@ -49,21 +48,23 @@ def odom(msg):
 	last_th = Odom.pose.pose.orientation.z
 	last_time = now_time
 
-	
+
 
 def Set():
-	rospy.Subscriber("/rpm_data", RPM2_Time, odom)
 	rospy.spin()
 
 if __name__=="__main__":
 	try:
 		rospy.init_node("Odom")
+		rospy.Subscriber("/rpm_data", RPM2_Time, odom)
 		pub=rospy.Publisher("/Odometry", Odometry, queue_size=2)
 		Odom=Odometry()
-		last_x=0.0
-		last_y=0.2
-		last_th=0.0
-		last_time=rospy.Time.now()
+
+		last_x = 0.0
+		last_y = 0.2
+		last_th = 0.0
+		last_time = rospy.Time.now()
+		start_Time = last_time
 		Set()
 
 	except rospy.ROSInterruptException: pass
