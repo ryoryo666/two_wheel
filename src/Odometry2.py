cfbfd92c849213ab15f4a0be5c55373fe4ea5b4f
ryoryo666 <sakaryo0102@gmail.com>
2021-01-14 15:52:12 +0900

    school

--- src/Odometry2.py
+++ src/Odometry2.py
@@ -12,11 +12,15 @@ d  = 0.0615 	# Wheel-Center distance [m]
 last_x = 0.0
 last_y = 0.0
 last_th = 0.0
+pre_v = 0.0
+pre_w = 0.0
 
 def odom(msg):
-    global last_x,last_y,last_th,last_Time,now_Time
+    global last_x, last_y, last_th, last_Time, now_Time, pre_v, pre_w
     R_data = msg.r_data
     L_data = msg.l_data
+#    print "R:{0}    L:{1}".format(R_data, L_data)
+#    print "R:{0}    L:{1}".format(R_data*(30/math.pi), L_data*(30/math.pi))
 
     now_Time = rospy.Time.now()
     dt = now_Time - last_Time
@@ -24,27 +28,31 @@ def odom(msg):
     t = now_Time - start_Time
     Odom.header.stamp.secs = t.secs
     Odom.header.stamp.nsecs = t.nsecs
+    print "t:{0}".format(t.secs + t.nsecs/10.0**9.0)
+#    print "dt:{0}".format(dt)
 
     vr = R_data * wr
     vl = L_data * wr
     v  = (vr+vl)/2.0
     w = (vr-vl)/(2.0*d)
+#    print "V:{0}    W:{1}".format(v, w)
 
-    Odom.pose.pose.position.x = last_x + v * dt * math.cos(Odom.pose.pose.orientation.z)
-    Odom.pose.pose.position.y = last_y + v * dt * math.sin(Odom.pose.pose.orientation.z)
-    Odom.pose.pose.orientation.z = last_th + w * dt
+    Odom.pose.pose.position.x +=  pre_v * dt * math.cos(Odom.pose.pose.orientation.z)
+    Odom.pose.pose.position.y +=  pre_v * dt * math.sin(Odom.pose.pose.orientation.z)
+    Odom.pose.pose.orientation.z += pre_w * dt
 
     Odom.twist.twist.linear.x = v
     Odom.twist.twist.angular.z = w
 
     print "x:" + str(Odom.pose.pose.position.x)
     print "y:" + str(Odom.pose.pose.position.y)
-    print "v:" + str(Odom.twist.twist.linear.x) + "\n"
+    print "θ:" + str(math.degrees(Odom.pose.pose.orientation.z))
+#    print "v:{0}".format(Odom.twist.twist.linear.x)
+    print ""
     pub.publish(Odom)
 
-    last_x = Odom.pose.pose.position.x
-    last_y = Odom.pose.pose.position.y
-    last_th = Odom.pose.pose.orientation.z
+    pre_v = v
+    pre_w = w
     last_Time = now_Time
 
 #def set():
