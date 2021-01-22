c2ababc5ad25226d7b0a388636bbef5f2c413653
ryoryo666 <sakaryo0102@gmail.com>
2021-01-22 13:16:26 +0900

    school

--- src/Speed_test.py
+++ src/Speed_test.py
@@ -10,21 +10,20 @@ def pub():
     rospy.init_node("Test_talker")
     pub=rospy.Publisher("New_cmd", RightLeft_cmd_value, queue_size=10)
     ref_v=RightLeft_cmd_value()
-    ref_v.r_ref = 1.0
-    ref_v.l_ref = 1.0
-    r=rospy.Rate(1)
-    i=0
-
-    start = raw_input("何か入力でスタート >> ")
-
-    if start != None:
-        while not rospy.is_shutdown():
-            pub.publish(ref_v)
-            r.sleep()
-            i+=1
-            if i ==5:
+    ref_v.r_ref = 1.0   #[rad/s]
+    ref_v.l_ref = 1.0   #[rad/s]
+    r = rospy.Rate(1)
+    i = 0
+    s = 0
+    
+    while s > 4():
+        pub.publish(ref_v)
+        r.sleep()
+        if i ==4:
                 ref_v.r_ref = 0.0
                 ref_v.l_ref = 0.0
+        i+=1
+        s += 1
 
 if __name__=="__main__":
     try:
